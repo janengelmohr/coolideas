4c92487f1ee64ef48d360945facaaa820826240a
jan <jan@jan-RC530-RC730>
2013-02-08 11:38:17 +0100

    +added state pattern

--- src/coolideas/JIdea.java
+++ src/coolideas/JIdea.java
@@ -11,41 +11,47 @@ import java.util.List;
  *
  * @author jan
  */
-public class JIdea {
+public class JIdea extends JContent {
     
     private List<JAttachment> attachments = new ArrayList<JAttachment>();
     private JState state;
+
+    public JIdea(String title, String description) {
+        super(title, description);
+        state = new Draft();
+    }
+    
     
     public void evaluate(JValuation b) {
-        
+        state.evaluate(b);
     }
     
     public void discuss(String text) {
-        
+        state.discuss(text);
     }
     
     public void hold() {
-        
+        state.hold();
     }
     
     public void release() {
-        
+        state.release();
     }
     
     public void decline() {
-        
+        state.decline();
     }
     
     public String getCurrentDiscussion() {
-        
+        return state.getCurrentDiscussion();
     }
     
     public JValuation getValuation() {
-        
+        return state.getValuation();
     }
     
     public void addAttachment(JAttachment a) {
-        
+        attachments.add(a);
     }
     
     public List<JAttachment> getAttachments() {
@@ -53,10 +59,70 @@ public class JIdea {
     }
     
     public void removeAttachment(JAttachment attachment) {
-        
+        attachments.remove(attachment);
     }
     
+    @Override
     public String toString() {
+        return "Idea: " + getTitle() + "\n" + getDescription();
+    }
+    
+    abstract class JState{
+    private JValuation valuation;
+    private String currentDiscussion;
+        public void discuss(String text) {
+            currentDiscussion = text;
+        }
         
+        public void evaluate(JValuation b) {
+        }
+        
+        public void hold() {
+        }
+        public void release() {
+        }
+        public void decline() {
+        }
+        public String getCurrentDiscussion() {
+            return currentDiscussion;
+        }
+        public JValuation getValuation() {
+            return valuation;
+        }
+    }
+    
+    class Draft extends JState {
+        @Override
+        public void hold() {
+            state = new OpenDraft();
+        }
+    }
+    
+    class OpenDraft extends JState {
+        @Override
+        public void discuss(String text) {
+            super.currentDiscussion = text;
+        }
+        @Override
+        public void evaluate(JValuation valuation) {
+            super.valuation = valuation;
+        }
+        @Override
+        public void hold() {
+            state = new ApprovedIdea();
+        }
+        @Override
+        public void decline() {
+        }
+    }
+    
+    class ApprovedIdea extends JState {
+        @Override
+        public void release() {
+            state = new Final();
+        }
+    }
+    class Final extends JState {
+        //fallback class
     }
 }
